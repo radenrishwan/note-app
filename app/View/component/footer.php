1ef17c2cc2c672c5a055cb170eaaaca9cc3ad04e
radenrishwan <radenrishwan@gmail.com>
2022-05-23 18:48:46 +0700

    add footer

--- app/View/component/footer.php
+++ app/View/component/footer.php
@@ -1,3 +1,13 @@
+<footer>
+    <div class="color-black footer">
+        <p class="copyright">
+            <span class="copyright-text">Copyright @ SeiorTech. 2022</span>
+            <br>
+            Build by : Raden Mohamad Rishwan
+            </p>
+    </div>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
